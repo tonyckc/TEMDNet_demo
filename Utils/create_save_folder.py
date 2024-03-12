2ac6fd4b81f6c52504e0a53848300797af0282b4
ck <33865803+tonyckc@users.noreply.github.com>
2024-03-12 11:20:51 +0800

    Delete Utils directory

--- Utils/create_save_folder.py
+++
@@ -1,7 +0,0 @@
-import os
-def create_save_folder(path):
-    if os.path.exists(path):
-        print('{} exists!\n'.format(path))
-    else:
-        os.makedirs(path)
-        print('{} create sucessfully\n'.format(path))
